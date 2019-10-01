111027c08bc3ede479282ec43c814eceb3993c0a
skrabacher <37253871+skrabacher@users.noreply.github.com>
2019-10-01 13:33:35 -0700

    added cursor focus to name input
    cursor will automatically focus on input element with name id

---
+++ the-form-element/js/script.js
@@ -0,0 +1,15 @@
+// ----------------------------------
+// Sarah Krabacher
+// FSJS TechDegree - Team Treahouse
+// Project 3
+// ----------------------------------
+
+// ***Put the first field in the `focus` state
+// ***● Use jQuery to select the 'Name' input element and place focus on it.
+
+$(document).ready(function() { //when page loads, cursor will automatically focus on input element with name id
+    $('#name').focus(); //
+});
+
+
+
